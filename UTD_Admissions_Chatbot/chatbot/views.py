b99a8bc739a958ec597d1834cfd3cae13e35537e
lanaejenkins1 <lanaejenkins1@gmail.com>
2023-02-24 23:33:17 -0600

    chatbot views mapped

--- UTD_Admissions_Chatbot/chatbot/views.py
+++ UTD_Admissions_Chatbot/chatbot/views.py
@@ -1,3 +1,12 @@
+import os
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from dotenv import load_dotenv
+from random import choice
+import openai
+from django.views import View
 
 # Create your views here.
+
+def chatbot(request):
+    return render(request, 'chatbot/chatbot.html')
