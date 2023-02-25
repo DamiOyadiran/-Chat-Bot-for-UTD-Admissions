fb0ff62afeddf3d5986920e0bcb5c31a2f4e9c27
lanaejenkins1 <lanaejenkins1@gmail.com>
2023-02-25 00:06:26 -0600

    fixed secret leak

--- UTD_Admissions_Chatbot/UTD_Admissions_Chatbot/settings.py
+++ UTD_Admissions_Chatbot/UTD_Admissions_Chatbot/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_x=uy&0x799@d1261grao!&rxyu21z^m+=$sh-xj29s65ft76_'
+load_dotenv()   # read the environment variables from .env file
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
