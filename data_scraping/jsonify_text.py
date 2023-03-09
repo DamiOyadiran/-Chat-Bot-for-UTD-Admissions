91ba8d99057b27c50796f2e79987cf27558c3b4c
AubreyFeldker <brf190002@utdallas.edu>
2023-03-08 21:10:02 -0600

    Redone base data .json to comply with OpenAI format

--- data_scraping/jsonify_text.py
+++ data_scraping/jsonify_text.py
@@ -4,21 +4,20 @@ directory = '../results'
 
 json_cont = { "input": "output" }
 
-for filename in os.listdir(directory):
-    file = open(os.path.join(directory, filename), 'r')
-    file_cont = file.read()
+with open("unstructured_data_v2.json", "a") as outfile:
+    for filename in os.listdir(directory):
+        file = open(os.path.join(directory, filename), 'r')
+        file_cont = file.read()
 
-    if (len(file_cont) < 2000):
-        
-        continue
+        if (len(file_cont) < 2000):
+            
+            continue
 
-    i = 0
-    while (len(file_cont) - (i * 1000) > 1000):
-        json_cont[file_cont[i*1000:i*1000+500] + " =>"] = file_cont[i*1000+500:i*1000+1000]
-        i+=1
-    #json_cont[file_cont[i*1000:round((i*1000 + len(file_cont))/2)] + " =>"] = file_cont[round((i*1000 + len(file_cont))/2):]
+        i = 0
+        while (len(file_cont) - (i * 1000) > 1000):
+            json_cont = {"prompt": file_cont[i*1000:i*1000+500] + " ->" }
+            json_cont["completion"] = file_cont[i*1000+500:i*1000+1000]
+            i+=1
 
-json_object = json.dumps(json_cont, indent=1)
-
-with open("unstructured_data.json", "w") as outfile:
-    outfile.write(json_object)
+            json_object = json.dumps(json_cont, indent=1)
+            outfile.write(json_object)
