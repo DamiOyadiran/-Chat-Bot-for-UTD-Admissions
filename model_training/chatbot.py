3dd6721100814c2a22c6dc6e0d46065e94de255e
lanaejenkins1 <lanaejenkins1@gmail.com>
2023-04-26 02:11:23 -0500

    requirements.txt

--- model_training/chatbot.py
+++ model_training/chatbot.py
@@ -21,16 +21,17 @@ response = openai.Completion.create(
 )
 
 def ask(question, chat_log=None):
- prompt_text = f’{chat_log}{restart_sequence}: {question}{start_sequence}:’
+ prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
  response = openai.Completion.create(
- engine=”davinci”,
+ engine="davinci",
  temperature=0.8,
  max_tokens=150,
  top_p=1,
  frequency_penalty=0,
  presence_penalty=0.3,
- stop=[“\n”],
+ stop=["\n"],
  )
 
  def append_interaction_to_chat_log(question, answer, chat_log=None):
-    if chat_log is None: chat_log = session_prompt return f’{chat_log}{restart_sequence} {question}{start_sequence}{answer}’
+    if chat_log is None: chat_log = session_prompt 
+    return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}'
