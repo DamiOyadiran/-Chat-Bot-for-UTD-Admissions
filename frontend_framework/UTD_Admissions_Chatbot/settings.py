b50ebdd9a3854a8100b433158413c4a5c9be751d
DamiOyadiran <40306566+DamiOyadiran@users.noreply.github.com>
2023-04-28 11:12:31 -0500

    more host code changes

--- frontend_framework/UTD_Admissions_Chatbot/settings.py
+++ frontend_framework/UTD_Admissions_Chatbot/settings.py
@@ -120,7 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = "app-root/repo/wsgi/static"
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    ('assets', 'app-root/repo/wsgi/openshift/static'),
+
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
