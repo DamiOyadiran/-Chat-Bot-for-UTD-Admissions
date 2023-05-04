731d9e9dbc1dd70569a8ccad53d174825b5fbc51
AubreyFeldker <brf190002@utdallas.edu>
2023-05-04 15:13:59 -0500

    updated comments, improved model prompt

--- model_training/question_answer_formation.py
+++ model_training/question_answer_formation.py
@@ -41,9 +41,9 @@ def get_answers(row):
     except:
         return ""
     
-def get_embedding(text: str, model: str="text-embedding-ada-002") -> list[float]:
+def get_embedding(text, model="text-embedding-ada-002"):
     global num_embed_get
-    if (num_embed_get != 0 and num_embed_get % 30 == 0):
+    if (num_embed_get != 0 and num_embed_get % 30 == 0): # Rate limitinggggggggggg
         print(f'sleep {num_embed_get / 60 + 1} started')
         time.sleep(90) # Getting around rate limiting :/
         print(f'sleep {num_embed_get / 60 + 1} completed')
@@ -57,16 +57,17 @@ def get_embedding(text: str, model: str="text-embedding-ada-002") -> list[float]
 
     return result["data"][0]["embedding"]
 
-def compute_doc_embeddings(df: pd.DataFrame) -> dict[tuple[str, str], list[float]]:
+def compute_doc_embeddings(df):
     return {
         idx: get_embedding(r.content) for idx, r in df.iterrows()
     }
 
 if __name__ == "__main__":
-    #df['questions']= df.context.apply(get_questions)
-    #df['answers']= df.apply(get_answers, axis=1)
-    #df.to_csv(os.path.join(os.path.dirname(__file__), INTERMEDIATE_FILE_NAME)) # File name passed as argument
+    df['questions']= df.context.apply(get_questions)
+    df['answers']= df.apply(get_answers, axis=1)
+    df.to_csv(os.path.join(os.path.dirname(__file__), INTERMEDIATE_FILE_NAME)) # File name passed as argument
 
+    # Saving to intermediate file just in case something happens during embedding creation, allows for easy recovery path with starting over
     df2 = pd.DataFrame.from_dict(compute_doc_embeddings(pd.read_csv(os.path.join(os.path.dirname(__file__), INTERMEDIATE_FILE_NAME))))
     df2 = pd.concat([df.filter(items=['topic', 'title']), df2.T], axis="columns")
     df2.to_csv(os.path.join(os.path.dirname(__file__), FINAL_FILE_NAME))
